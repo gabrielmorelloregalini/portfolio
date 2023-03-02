3877a830685786b2c284ba2582566e16fa4aa0d8
gabrielmorelloregalini <gabrielmorelloregalini000@gmail.com>
2023-03-02 04:04:10 -0300

    guardar1

---
+++ js/tipeo.js
@@ -0,0 +1,12 @@
+
+const typed1 = new Typed('.typed1', {
+  strings: ['Lorem ipsum dolor sit amet, consectetur adipiscing'],
+  typeSpeed: 30,
+  backSpeed: 0,
+});
+
+const typed2 = new Typed('.typed2', {
+  strings: ['Lorem ipsum dolor sit amet, consectetur '],
+  typeSpeed: 30,
+  backSpeed: 0,
+});
