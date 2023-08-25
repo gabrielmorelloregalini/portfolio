bcc0a957c556aa83bd8a760693998c8699092c77
gabrielmorelloregalini <gabrielmorelloregalini000@gmail.com>
2023-08-24 21:07:56 -0300

    a

--- js/tipeo.js
+++ js/tipeo.js
@@ -1,13 +1,10 @@
 
 const typed1 = new Typed('.typed1', {
-  strings: ['Lorem ipsum dolor sit amet, consectetur adipiscing'],
-  typeSpeed: 30,
-  backSpeed: 0,
+  strings: ["Las virtudes que resaltan en mi incluyen creatividad en la resolución de desafíos, empatía en la conexión con otros, persistencia en la consecución de objetivos, y valoración de la honestidad en las relaciones. Asimismo, adopto una perspectiva optimista y tengo habilidades de liderazgo. Mantengo la calma en situaciones difíciles, valoro la colaboración y busco la autenticidad personal. Mi deseo de aprender constantemente también es una virtud clave."],
+  typeSpeed: 20,
 });
 
 const typed2 = new Typed('.typed2', {
-  strings: ['Lorem ipsum dolor sit amet, consectetur '],
+  strings: ['Dentro de mis habilidades se destacan por un lado Front-End: Interfaces HTML/CSS, adaptación a dispositivos, integración con APIs, validación de entrada. Y por otro lado el Back-End: Capa de datos con Hibernate y Spring Data, servicios expuestos en APIs REST, manejo de excepciones, pruebas unitarias e integración con Log4j.'],
   typeSpeed: 30,
-  backSpeed: 0,
-  loop: true
 });
